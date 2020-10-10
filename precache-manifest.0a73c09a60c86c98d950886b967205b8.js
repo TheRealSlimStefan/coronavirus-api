316f7213d14b43918717f7bd0e7eeefe8f4a9276
Marcin Stefanowicz <19smarcins98@gmail.com>
2020-10-10 16:49:24 +0200

    Updates

---
+++ precache-manifest.0a73c09a60c86c98d950886b967205b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "595d04ae1dd4958102177215894ef081",
+    "url": "/coronavirus-api/index.html"
+  },
+  {
+    "revision": "084714a5e0fe275fff03",
+    "url": "/coronavirus-api/static/css/main.97a35a67.chunk.css"
+  },
+  {
+    "revision": "58d6210aa81a20663d71",
+    "url": "/coronavirus-api/static/js/2.58a2d568.chunk.js"
+  },
+  {
+    "revision": "4acd90554931a5346c73c3af099efcb8",
+    "url": "/coronavirus-api/static/js/2.58a2d568.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "084714a5e0fe275fff03",
+    "url": "/coronavirus-api/static/js/main.b2ed3fea.chunk.js"
+  },
+  {
+    "revision": "c5d6021f2daecbb38036",
+    "url": "/coronavirus-api/static/js/runtime-main.5112696f.js"
+  }
+]);
